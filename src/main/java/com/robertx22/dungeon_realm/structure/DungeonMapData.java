ab2a6de64f907563697977e45c3599b1881992b2
SaloEater <danil.lazarev@gmail.com>
2025-11-05 12:29:34 -0600

    Revert "fixes after merge"
    This reverts commit 897983f40af0f1aa76c93976375803567f1e78f0.

--- src/main/java/com/robertx22/dungeon_realm/structure/DungeonMapData.java
+++ src/main/java/com/robertx22/dungeon_realm/structure/DungeonMapData.java
@@ -221,8 +221,8 @@ public class DungeonMapData {
         for (Player p : DungeonMain.MAIN_DUNGEON_STRUCTURE.getAllPlayersInMap(level, pos)) {
             Scoreboard scoreboard = p.getScoreboard();
             Objective completionPercentObjective = getCompletionPercentObjective(scoreboard);
-            showCompletionScore(p, completionPercentObjective, killedMobsPercent);
-            positionScoreboard(p, completionPercentObjective);
+            showCompletionScore(scoreboard, completionPercentObjective, killedMobsPercent);
+            positionScoreboard(scoreboard, completionPercentObjective);
         }
     }
 
